fb65c4e64e03923de4d1f38a16fb54bbda3efd9e
ModderPenguin <modderpenguin@gmail.com>
2013-05-01 20:18:24 +1000

    Deleted some stuff

---
+++ source/minepg/rpg_common/rpg/PlayerClassHandler.java
@@ -0,0 +1,24 @@
+package rpg;
+
+import java.io.File;
+
+import net.minecraft.client.Minecraft;
+
+public class PlayerClassHandler {
+	
+	private static String playersClass;
+	public static File playerClassFile;
+	
+	public static void setPlayersClass(String string) {
+		playersClass = string;
+	}
+	
+	public static String getPlayersClass() {
+		return playersClass;
+	}
+	
+	public static boolean hasPlayerClassFile() {
+		playerClassFile = new File(DownloadHelper.getDir() + "/saves/" + Minecraft.getMinecraft().theWorld.getSaveHandler().getWorldDirectoryName() + "Random.txt");
+		return playerClassFile.exists();
+	}
+}
