3c2c3dfc72376add6d5690f7eec765b4214c9547
ModderPenguin <modderpenguin@gmail.com>
2013-05-06 20:06:44 +1000

    Started working on some creative tabs

---
+++ source/minepg/rpg_common/rpg/config/RPGCreativeTabs.java
@@ -0,0 +1,9 @@
+package rpg.config;
+
+import net.minecraft.creativetab.CreativeTabs;
+import rpg.creativetab.CreativeTabArcherItems;
+
+public class RPGCreativeTabs {
+	
+	public static final CreativeTabs tabArcherItems = new CreativeTabArcherItems("archerItems");
+}
---
+++ source/minepg/rpg_common/rpg/creativetab/CreativeTabArcherItems.java
@@ -0,0 +1,16 @@
+package rpg.creativetab;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.ItemStack;
+import rpg.config.base.archer.ArcherWeapons;
+
+public class CreativeTabArcherItems extends CreativeTabs {
+
+	public CreativeTabArcherItems(String label) {
+		super(label);
+	}
+	
+	public ItemStack getIconItemStack() {
+        return new ItemStack(ArcherWeapons.bowTraining, 1);
+}
+}
