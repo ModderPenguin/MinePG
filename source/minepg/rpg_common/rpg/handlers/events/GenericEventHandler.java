006a7ea34a86ad60fd308dc70fe9485bf2e667b2
ModderPenguin <modderpenguin@gmail.com>
2013-05-12 01:43:08 +1000

    Changed the name of some textures so that they actually work

---
+++ source/minepg/rpg_common/rpg/handlers/events/GenericEventHandler.java
@@ -0,0 +1,34 @@
+package rpg.handlers.events;
+
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraftforge.event.ForgeSubscribe;
+import net.minecraftforge.event.entity.EntityEvent;
+import rpg.playerinfo.PlayerInformation;
+import cpw.mods.fml.common.IPlayerTracker;
+
+
+public class GenericEventHandler implements IPlayerTracker  {
+    
+    @ForgeSubscribe
+    public void onEntityConstruct(EntityEvent.EntityConstructing event) {
+        if (event.entity instanceof EntityPlayer) {
+            event.entity.registerExtendedProperties(PlayerInformation.IDENTIFIER, new PlayerInformation((EntityPlayer) event.entity));
+        }
+    }
+    
+    @Override
+    public void onPlayerChangedDimension(EntityPlayer player) {
+        PlayerInformation.forPlayer(player).setDirty();
+    }
+
+    @Override
+    public void onPlayerRespawn(EntityPlayer player) {
+        PlayerInformation.forPlayer(player).setDirty();
+    }
+
+    @Override
+    public void onPlayerLogin(EntityPlayer player) {}
+
+    @Override
+    public void onPlayerLogout(EntityPlayer player) {}
+}
