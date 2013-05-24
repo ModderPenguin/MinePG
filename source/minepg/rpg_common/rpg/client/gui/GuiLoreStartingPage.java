6c3107db9244974731d4d833a95270108c571082
ModderPenguin <modderpenguin@gmail.com>
2013-05-24 14:26:30 +1000

    Updated where the writing is placed in the lore GUI

--- source/minepg/rpg_common/rpg/client/gui/GuiLoreStartingPage.java
+++ source/minepg/rpg_common/rpg/client/gui/GuiLoreStartingPage.java
@@ -24,32 +24,32 @@ public class GuiLoreStartingPage extends GuiContainer {
         drawString(
                 fontRenderer,
                 "Steve was bored. He had slain the great dragon, which had guarded the dark abyss",
-                2, 10, 0xFFFFFF);
-        drawString(
-                fontRenderer,
-                "of the End. He had gone to the molten depths of the underworld. He watched the",
-                2, 20, 0xFFFFFF);
-        drawString(
-                fontRenderer,
-                "sunset from his mansion and thought to himself, where did my adventure go? Not ",
-                3, 30, 0xFFFFFF);
-        drawString(
-                fontRenderer,
-                "one to wait for an update for new stuff, he took action. He prepared to go out",
                 2, 40, 0xFFFFFF);
         drawString(
                 fontRenderer,
-                "and find new adventures, new battles, new purposes for the life he lives. What",
+                "of the End. He had gone to the molten depths of the underworld. He watched the",
                 2, 50, 0xFFFFFF);
         drawString(
                 fontRenderer,
+                "sunset from his mansion and thought to himself, where did my adventure go? Not ",
+                3, 60, 0xFFFFFF);
+        drawString(
+                fontRenderer,
+                "one to wait for an update for new stuff, he took action. He prepared to go out",
+                2, 70, 0xFFFFFF);
+        drawString(
+                fontRenderer,
+                "and find new adventures, new battles, new purposes for the life he lives. What",
+                2, 80, 0xFFFFFF);
+        drawString(
+                fontRenderer,
                 "he did not know was a great evil was growing, and he needs all the luck he can",
-                2, 60, 0xFFFFFF);
+                2, 90, 0xFFFFFF);
         drawString(fontRenderer,
                 "get to stop this great darkness that plagues Minecraftia.", 2,
                 70, 0xFFFFFF);
         drawString(fontRenderer, "Now your story starts here...... good luck.",
-                2, 90, 0xFFFFFF);
+                2, 100, 0xFFFFFF);
     }
 
     @Override
